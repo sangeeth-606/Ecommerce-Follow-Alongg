80408fbb301302dea2acfb51f6a2022db0c5c435
sangeeth-606 <sangeeth999123@gmail.com>
2025-01-20 14:04:01 +0000

    Add .env file for MongoDB connection string

--- server/server.js
+++ server/server.js
@@ -1,32 +1,29 @@
-const express=require('express');
-const mongoose=require('mongoose');
+// filepath: /home/zape/sange/Desktop/Ecommerce-Follow-Alongg/server/server.js
+const express = require('express');
+const mongoose = require('mongoose');
 const uploadRoutes = require('./routes/uploadRoutes');
-
-
-
 const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
 
-const app=express()
-const PORT=8084
+const app = express();
+const PORT = 8084;
 app.use(express.json());
 
-let connection= mongoose.connect("mongodb+srv://sangeethdrive69:gXIGaQ1sK2fgCRRi@cluster0.iojyc.mongodb.net/")
+let connection = mongoose.connect(process.env.MONGODB_URI);
 
-app.get("/ping",(req,res)=>{
-    res.send("pong")
-})
+app.get("/ping", (req, res) => {
+    res.send("pong");
+});
 app.use('/', userRoutes);
 app.use('/', uploadRoutes);
 
+app.listen(PORT, async () => {
+    try {
+        await connection;
+        console.log("Successfully connected to MongoDB");
+    } catch (error) {
+        console.log(error);
+    }
 
-
-app.listen(PORT,async()=>{
-try{
-    await connection;
-    console.log("Successfully connected to MongoDB");
-} catch (error){
-    console.log(error);
-}
-
-    console.log(`Server is running on port ${PORT}`)
-})
+    console.log(`Server is running on port ${PORT}`);
+});
