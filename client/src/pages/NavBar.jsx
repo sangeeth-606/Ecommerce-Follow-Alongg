5af276cb0cbe9a295441c9ede328b2350da800b0
sangeeth-606 <sangeeth999123@gmail.com>
2025-03-20 14:57:58 +0530

    Update NavBar styles for improved visual consistency and hover effects

--- client/src/pages/NavBar.jsx
+++ client/src/pages/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="bg-gray-800 p-4 shadow-lg fixed w-full top-0 z-50">
+    <nav className="bg-gradient-to-r from-slate-800 via-slate-900 to-slate-950 p-4 shadow-lg fixed w-full top-0 z-50">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <Link to="/" className="text-white text-2xl font-bold mb-4 md:mb-0">
           E-Commerce
@@ -13,41 +13,41 @@ const NavBar = () => {
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
           <Link
             to="/my-orders"
-            className="text-white hover:bg-gray-700 px-4 py-2 rounded transition-colors"
+            className="text-white/80 hover:bg-slate-700 px-4 py-2 rounded transition-colors"
           >
             My Orders
           </Link>
 
           <Link
             to="/"
-            className="text-white hover:bg-gray-700 px-4 py-2 rounded transition-colors"
+            className="text-white/80 hover:bg-slate-700 px-4 py-2 rounded transition-colors"
           >
             My Products
           </Link>
 
           <Link
             to="/add-product"
-            className="text-white hover:bg-gray-700 px-4 py-2 rounded transition-colors"
+            className="text-white/80 hover:bg-slate-700 px-4 py-2 rounded transition-colors"
           >
             Add Product
           </Link>
 
           <Link
             to="/cart-page"
-            className="text-white hover:bg-gray-700 px-4 py-2 rounded transition-colors"
+            className="text-white/80 hover:bg-slate-700 px-4 py-2 rounded transition-colors"
           >
             Cart
           </Link>
 
           <Link
             to="/login"
-            className="text-white hover:bg-gray-700 px-4 py-2 rounded transition-colors"
+            className="text-white/80 hover:bg-slate-700 px-4 py-2 rounded transition-colors"
           >
             Login
           </Link>
           <Link
             to="/profile"
-            className="text-white hover:bg-gray-700 px-4 py-2 rounded transition-colors flex items-center gap-1"
+            className="text-white/80 hover:bg-slate-700 px-4 py-2 rounded transition-colors flex items-center gap-1"
           >
             <CgProfile className="text-xl" />
             Profile
